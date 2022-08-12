149cccfc43f4d542f3cd8250211f217b71375702
Darshan Vaishya <DarshanVaishya3@gmail.com>
2022-08-13 01:45:34 +0530

    Style: Fixed container issue

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -35,9 +35,7 @@ function Header() {
 				</Container>
 			</AppBar>
 
-			<Container>
-				<Outlet />
-			</Container>
+			<Outlet />
 		</>
 	);
 }
--- src/routes/home-page/home-page.component.jsx
+++ src/routes/home-page/home-page.component.jsx
@@ -1,12 +1,13 @@
+import { Container } from "@mui/material";
 import Banner from "../../components/banner/banner.component";
 import CoinsTable from "../../components/coins-table/coins-table.component";
 
 function HomePage() {
 	return (
-		<>
+		<Container>
 			<Banner />
 			<CoinsTable />
-		</>
+		</Container>
 	);
 }
 
